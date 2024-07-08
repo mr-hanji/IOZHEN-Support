1c0a843ebbd951804948921eb7747e27bae96052
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-07-08 12:44:33 +0330

    fix message for form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,127 +1,142 @@
 import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [description, setDescription] = useState("");
+   const [rating, setRating] = useState(0);
+
+   const handleRating = (rate) => {
+      setRating(rate);
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const formData = {
+         firstName,
+         lastName,
+         score: rating,
+         description,
+      };
+
+      try {
+         const response = await axios.post(
+            "http://localhost:3000/api/tickets",
+            formData
+         );
+         if (response.status === 200 || response.status === 201) {
+            toast.success("Your ticket was sent successfully!");
+         }
+      } catch (error) {
+         toast.error("Error submitting the form, please try again.");
+      }
+   };
+
    return (
       <>
-         <div class="bg-white border border-4 rounded-lg shadow relative m-10">
-            <div class="flex items-start justify-between p-5 border-b rounded-t">
+         <ToastContainer />
+         <div className="relative m-10 bg-white border border-4 rounded-lg shadow">
+            <div className="flex items-start justify-between p-5 border-b rounded-t">
                <div className="flex items-center gap-[10px] justify-center">
                   <img src="/logo.jpg" alt="" className="w-[80px] h-[80px]" />
-                  <h3 class="text-xl font-semibold">IOZHEN</h3>
+                  <h3 className="text-xl font-semibold">IOZHEN</h3>
                </div>
-               <button
-                  type="button"
-                  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                  data-modal-toggle="product-modal"
-               >
-                  <svg
-                     class="w-5 h-5"
-                     fill="currentColor"
-                     viewBox="0 0 20 20"
-                     xmlns="http://www.w3.org/2000/svg"
-                  >
-                     <path
-                        fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                     ></path>
-                  </svg>
-               </button>
+               <h3 className="text-center absolute left-[35%] text-[30px] top-[5%]">
+                  IOZHEN SUPPORT FORM
+               </h3>
             </div>
 
-            <div class="p-6 space-y-6">
-               <form action="#">
-                  <div class="grid grid-cols-6 gap-6">
-                     <div class="col-span-6 sm:col-span-3">
+            <div className="p-6 space-y-6">
+               <form onSubmit={handleSubmit}>
+                  <div className="grid grid-cols-6 gap-6">
+                     <div className="col-span-6 sm:col-span-3">
                         <label
-                           htmlFor="product-name"
-                           class="text-sm font-medium text-gray-900 block mb-2"
+                           htmlFor="first-name"
+                           className="block mb-2 text-sm font-medium text-gray-900"
                         >
-                           Product Name
+                           First Name
                         </label>
                         <input
                            type="text"
-                           name="product-name"
-                           id="product-name"
-                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                           placeholder="Apple Imac 27”"
-                           required=""
+                           name="first-name"
+                           id="first-name"
+                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                           placeholder="Enter your First Name"
+                           required
+                           value={firstName}
+                           onChange={(e) => setFirstName(e.target.value)}
                         />
                      </div>
-                     <div class="col-span-6 sm:col-span-3">
+                     <div className="col-span-6 sm:col-span-3">
                         <label
-                           htmlFor="category"
-                           class="text-sm font-medium text-gray-900 block mb-2"
+                           htmlFor="last-name"
+                           className="block mb-2 text-sm font-medium text-gray-900"
                         >
-                           Category
+                           Last Name
                         </label>
                         <input
                            type="text"
-                           name="category"
-                           id="category"
-                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                           placeholder="Electronics"
-                           required=""
+                           name="last-name"
+                           id="last-name"
+                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+                           placeholder="Enter your Last Name"
+                           required
+                           value={lastName}
+                           onChange={(e) => setLastName(e.target.value)}
                         />
                      </div>
-                     <div class="col-span-6 sm:col-span-3">
+                     <div className="col-span-full">
                         <label
-                           for="brand"
-                           class="text-sm font-medium text-gray-900 block mb-2"
+                           htmlFor="description"
+                           className="block mb-2 text-sm font-medium text-gray-900"
                         >
-                           Brand
-                        </label>
-                        <input
-                           type="text"
-                           name="brand"
-                           id="brand"
-                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                           placeholder="Apple"
-                           required=""
-                        />
-                     </div>
-                     <div class="col-span-6 sm:col-span-3">
-                        <label
-                           htmlFor="price"
-                           class="text-sm font-medium text-gray-900 block mb-2"
-                        >
-                           Price
-                        </label>
-                        <input
-                           type="number"
-                           name="price"
-                           id="price"
-                           class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-                           placeholder="$2300"
-                           required=""
-                        />
-                     </div>
-                     <div class="col-span-full">
-                        <label
-                           htmlFor="product-details"
-                           class="text-sm font-medium text-gray-900 block mb-2"
-                        >
-                           Product Details
+                           Description
                         </label>
                         <textarea
-                           id="product-details"
+                           id="description"
                            rows="6"
-                           class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-4"
+                           className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-sm focus:ring-cyan-600 focus:border-cyan-600"
                            placeholder="Details"
+                           value={description}
+                           onChange={(e) => setDescription(e.target.value)}
                         ></textarea>
                      </div>
+                     <div className="col-span-full">
+                        <label className="block mb-2 text-sm font-medium text-gray-900">
+                           Rating
+                        </label>
+                        <div className="flex gap-1">
+                           {[1, 2, 3, 4, 5].map((star) => (
+                              <button
+                                 type="button"
+                                 key={star}
+                                 className={`text-xl ${
+                                    rating >= star
+                                       ? "text-yellow-400"
+                                       : "text-gray-400"
+                                 }`}
+                                 onClick={() => handleRating(star)}
+                              >
+                                 ★
+                              </button>
+                           ))}
+                        </div>
+                     </div>
+                  </div>
+                  <div className="p-6 border-t border-gray-200 rounded-b">
+                     <button
+                        className="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                        type="submit"
+                     >
+                        Save all
+                     </button>
                   </div>
                </form>
-            </div>
-
-            <div class="p-6 border-t border-gray-200 rounded-b">
-               <button
-                  class="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                  type="submit"
-               >
-                  Save all
-               </button>
             </div>
          </div>
       </>
